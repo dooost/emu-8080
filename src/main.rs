6fd90ce250aa8769ecb7a2db3adc27bb1db6e884
Basir Doost <basir.doost@prezi.com>
2021-09-18 01:34:29 +0200

    Minor output improvements

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,56 @@ use std::{convert::TryFrom, fs};
 mod disassembler;
 
 fn main() {
-    let buf = fs::read("/Users/prezi/Developer/emu-8080/resources/invaders.h")
+    let mut buf = vec![];
+
+    let mut buf_h = fs::read("/Users/prezi/Developer/emu-8080/resources/invaders.h")
         .expect("Failed to read file");
+    let mut buf_g = fs::read("/Users/prezi/Developer/emu-8080/resources/invaders.g")
+        .expect("Failed to read file");
+    let mut buf_f = fs::read("/Users/prezi/Developer/emu-8080/resources/invaders.f")
+        .expect("Failed to read file");
+    let mut buf_e = fs::read("/Users/prezi/Developer/emu-8080/resources/invaders.e")
+        .expect("Failed to read file");
+
+    buf.append(&mut buf_h);
+    buf.append(&mut buf_g);
+    buf.append(&mut buf_f);
+    buf.append(&mut buf_e);
+
     println!("Start...");
 
     let mut output = String::new();
 
     let mut iter = buf.iter();
+    let mut counter: usize = 0;
     while let Some(byte) = iter.next() {
+        let hex_counter = format!("{:04x}", counter);
         let hex_byte = format!("{:#04x}", byte);
 
-        let mut output_line = format!("{}", hex_byte);
+        let mut output_line = format!("{}    {}", hex_counter, hex_byte);
+
+        counter += 1;
 
         let instruction = Instruction::try_from(*byte);
-
         match instruction {
             Ok(instruction) => {
                 output_line = format!("{}    {}", output_line, instruction.to_string());
 
+                let mut next_bytes = vec![];
                 for _i in 1..instruction.size() {
                     let byte = iter.next().expect("Unterminated instruction");
-                    let hex_byte = format!("{:#04x}", byte);
+                    next_bytes.push(byte);
+                    counter += 1;
+                }
 
-                    output_line = format!("{}    {}", output_line, hex_byte);
+                let mut next_bytes_iter = next_bytes.iter();
+                if next_bytes.len() == 1 {
+                    let adr_str = format!("#${:02x}", next_bytes_iter.next().unwrap());
+                    output_line = format!("{}    {}", output_line, adr_str);
+                } else if next_bytes.len() == 2 {
+                    let mut adr_str = format!("{:02x}", next_bytes_iter.next().unwrap());
+                    adr_str = format!("${:02x}{}", next_bytes_iter.next().unwrap(), adr_str);
+                    output_line = format!("{}    {}", output_line, adr_str);
                 }
             }
             Err(()) => (),
@@ -36,6 +63,6 @@ fn main() {
         output.push_str(&output_line);
     }
 
-    fs::write("/Users/prezi/Developer/emu-8080/invaders-h.txt", output)
+    fs::write("/Users/prezi/Developer/emu-8080/invaders.txt", output)
         .expect("Failed to write output file");
 }
