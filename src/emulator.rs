64de1d99f6ff8721a80ffa292a9c740d8035b11f
Basir Doost <basir.doost@prezi.com>
2021-10-04 23:04:02 +0200

    Add features for logging and diag

--- src/emulator.rs
+++ src/emulator.rs
@@ -206,6 +206,8 @@ impl State8080 /*<'a>*/ {
     }
 
     fn evaluating_instruction(self, instruction: Instruction) -> Self {
+        #[cfg(feature = "logging")]
+        #[cfg(not(feature = "diagsupport"))]
         self.log_instruction(instruction.clone());
 
         let mut state = self;
